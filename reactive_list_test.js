579aff806cdf102fbb7c1c25283d6a70d4597af6
Sam Corcos <samcorcos@gmail.com>
2014-12-13 19:20:07 -0500

    unsecure person list, edit, add, remove functionality

--- reactive_list_test.js
+++ reactive_list_test.js
@@ -1,39 +1,49 @@
+var People = new Mongo.Collection("people");
+
 if (Meteor.isClient) {
+  // Template.personList.people = function() {
+  //   return People.find();
+  // }
+  // These two are the same thing.
+  Template.personList.helpers({
+    people: function() {
+      return People.find();
+    }
+  })
 
-  Session.set("listItems", ["test", "anoterh test"])
+  Template.personForm.events({
+    'click button': function(e,t) {
+      var el = t.find("#name");
+      People.insert({ name: el.value});
+      el.value = "";
+    }
+  })
 
-  Template.list.listItems = function() {
-    return Session.get("listItems");
-  }
+  Template.person.helpers({
+    editing: function() {
+      return Session.get("edit-" + this._id);
+    }
+  })
 
-  function add(item) {
-    var list = Session.get("listItems");
-    list.push(item);
-    Session.set("listItems", list);
-  }
-
-  Template.list.events({
-    "keypress input": function(e,t) {
+  Template.person.events({
+    'click .name': function(e,t) {
+      Session.set("edit-" + t.data._id, true);
+    },
+    'keypress input': function(e,t) {
       if (e.keyCode === 13) {
-        var input = t.find("input");
-        add(input.value);
-        input.value = "";
+        // Old code that has been replaced by lones 36 and 37:
+        // Person.update(t.data, {$set: {name: e.currentTarget.value }});      // The reason for this is, "For code that runs on the client side/browser side you can only use an _id field as the query. On the server you can run it as you please.""
+        var person = People.findOne(t.data);
+        People.update({_id: person._id}, {$set: {name: e.currentTarget.value}});
+        Session.set("edit-" + t.data._id, false);
       }
+    },
+    'click .del': function(e,t) {
+      var person = People.findOne(t.data);
+      People.remove(person._id);
     }
   })
 
-  function remove(item) {
-    var list = Session.get("listItems");
-    i = list.indexOf(item);
-    if (i > -1) {
-      Session.set("listItems", list.slice(0,i).concat(list.slice(i+1)));
-    }
-  }
 
-  Template.item.events({
-    "click": function(e,t) {
-      remove(t.data);
-    }
-  })
 
 }
